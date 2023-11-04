c76124815b468ce06eedf1c636d1558135fcb979
BokeChinmay <chinmaygaikwad2004@gmail.com>
2023-11-04 13:04:02 -0400

    messing around like a goofball

--- Visual-Art/sketch.js
+++ Visual-Art/sketch.js
@@ -1,37 +1,24 @@
-let img;
-let vid;
-let theta = 0;
+let xList = [];
+let yList = [];
+
+let x = 0;
+let y = 0;
 
 function setup() {
-  createCanvas(710, 400, WEBGL);
-
-  img = loadImage('assets/cat.jpg');
-  vid = createVideo(['assets/360video_256crop_v2.mp4']);
-  vid.elt.muted = true;
-  vid.loop();
-  vid.hide();
+  createCanvas(710, 400);
 }
 
 
 
 function draw() {
-  background(250);
-  translate(-220, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  //pass image as texture
-  texture(vid);
-  sphere(150);
-  pop();
-  translate(440, 0, 0);
-  push();
-  rotateZ(theta * 0.1);
-  rotateX(theta * 0.1);
-  rotateY(theta * 0.1);
-  texture(img);
-  box(100, 100, 100);
-  pop();
-  theta += 0.05;
+  background(100);
+  for(let i = 0; i < 20; i++) {
+    xList.push(ellipse(x, i, 20, 20));
+  }
+  for(let i = 0; i < 20; i++) {
+    xList.push(ellipse(i, y, 20, 20));
+  }
+
+  x++;
+  y++;
 }
